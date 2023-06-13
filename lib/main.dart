31b57f9e9aa2e0dd9156f3a98a3c432053f4bd4c
falleiro <caiofalleiro@gmail.com>
2023-06-13 17:35:59 -0300

    finalizando primeira parte de api

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import 'models/journal.dart';
+import 'screens/add_journal_screen/add_journal_screen.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
+
+  JournalService journalService = JournalService();
+  // journalService.register(Journal.empty());
+  journalService.getAll();
 }
 
 class MyApp extends StatelessWidget {
@@ -13,20 +19,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.grey,
         appBarTheme: const AppBarTheme(
           elevation: 0,
           backgroundColor: Colors.black,
           titleTextStyle: TextStyle(color: Colors.white),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white),
         ),
-        textTheme: GoogleFonts.bitterTextTheme(),
       ),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.light,
       initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
+      },
+      onGenerateRoute: (routeSettings) {
+        if (routeSettings.name == "add-journal") {
+          final journal = routeSettings.arguments as Journal;
+          return MaterialPageRoute(
+            builder: (context) {
+              return AddJournalScreen(journal: journal);
+            },
+          );
+        }
+        return null;
       },
     );
   }
